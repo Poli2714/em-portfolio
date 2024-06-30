e4d6c6cd9c30b77e7e91b15f18f993527ec8972e
Elgun Mehdi <elgun88@gmail.com>
2024-06-30 08:02:29 -0600

    feat: create BlogPostMetaInfo shared component

---
+++ src/components/ui/BlogPostMetaInfo/BlogPostMetaInfo.test.tsx
@@ -0,0 +1,15 @@
+import { expect, test } from 'vitest';
+import { render, screen } from '@testing-library/react';
+
+import BlogPostMetaInfo from './BlogPostMetaInfo';
+
+test('renders BlogPostMetaInfo', () => {
+  render(<BlogPostMetaInfo author='Elgun' date='June 30, 2024' readTime={5} />);
+
+  expect(
+    screen.getByRole('heading', { level: 4, name: 'Elgun' })
+  ).toBeInTheDocument();
+  expect(screen.getByRole('paragraph').textContent).toMatch(
+    /^June 30, 2024.*5 min read$/
+  );
+});
---
+++ src/components/ui/BlogPostMetaInfo/BlogPostMetaInfo.tsx
@@ -0,0 +1,24 @@
+import { ImageIcon } from 'lucide-react';
+
+type BlogPostMetaInfoProps = {
+  author: string;
+  date: string;
+  readTime: number;
+};
+
+function BlogPostMetaInfo({ author, date, readTime }: BlogPostMetaInfoProps) {
+  return (
+    <div className='grid grid-cols-[min-content,1fr] grid-rows-2 gap-x-4'>
+      <div className='row-span-2 flex h-12 w-12 items-center justify-center rounded-full bg-primary/15'>
+        <ImageIcon className='text-foreground/30' size={20} strokeWidth={1} />
+      </div>
+      <h4 className='font-medium'>{author}</h4>
+      <p className='flex items-center gap-x-2 text-sm'>
+        {date} <span className='h-1 w-1 rounded-full bg-foreground'></span>{' '}
+        {readTime} min read
+      </p>
+    </div>
+  );
+}
+
+export default BlogPostMetaInfo;
