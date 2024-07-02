bd37dcec564e3aa3c03f96fa599ba09988a8118f
Elgun Mehdi <elgun88@gmail.com>
2024-07-02 17:13:17 -0600

    feat: create shared BlogPostSection component

---
+++ src/app/(routes)/blog/[slug]/components/BlogPostSection/BlogPostSection.test.tsx
@@ -0,0 +1,91 @@
+import { expect, test, vi } from 'vitest';
+import { render, screen } from '@testing-library/react';
+
+import BlogPostSection from './BlogPostSection';
+
+vi.mock('@/components/shadcn', () => ({
+  AspectRatio: () => <div data-testid='photo'>Mock photo</div>,
+}));
+
+vi.mock('@/components/ui', () => ({
+  Photo: () => <div>Mock photo content</div>,
+}));
+
+test('renders BlogPostSection with only paragraphs when code, subtitle and image are not provided', () => {
+  render(
+    <BlogPostSection
+      code={undefined}
+      image={undefined}
+      paragraphs={['Test paragraph1', 'Test paragraph2']}
+      subtitle={undefined}
+    />
+  );
+  const paragraphs = screen.getAllByRole('paragraph');
+
+  expect(screen.queryByRole('heading', { level: 3 })).not.toBeInTheDocument();
+
+  expect(paragraphs).toHaveLength(2);
+  paragraphs.forEach((paragraph, i) => {
+    expect(paragraph).toHaveTextContent(`Test paragraph${i + 1}`);
+  });
+
+  expect(screen.queryByRole('code')).not.toBeInTheDocument();
+  expect(screen.queryByTestId('photo')).not.toBeInTheDocument();
+});
+
+test('renders BlogPostSection with subtitle and paragraph when code and image are not provided', () => {
+  render(
+    <BlogPostSection
+      code={undefined}
+      image={undefined}
+      paragraphs={['Test paragraph']}
+      subtitle='Test subtitle'
+    />
+  );
+
+  expect(
+    screen.getByRole('heading', { level: 3, name: 'Test subtitle' })
+  ).toBeInTheDocument();
+  expect(screen.getByRole('paragraph')).toHaveTextContent(/^test paragraph$/i);
+  expect(screen.queryByRole('code')).not.toBeInTheDocument();
+  expect(screen.queryByTestId('photo')).not.toBeInTheDocument();
+});
+
+test('renders BlogPostSection with subtitle, paragraph and code when image is not provided', () => {
+  render(
+    <BlogPostSection
+      code={`console.log('Hello world!');`}
+      image={undefined}
+      paragraphs={['Test paragraph']}
+      subtitle='Test subtitle'
+    />
+  );
+
+  expect(
+    screen.getByRole('heading', { level: 3, name: 'Test subtitle' })
+  ).toBeInTheDocument();
+  expect(screen.getByRole('paragraph')).toHaveTextContent(/^test paragraph$/i);
+  expect(screen.getByRole('code')).toBeInTheDocument();
+  expect(screen.queryByTestId('photo')).not.toBeInTheDocument();
+});
+
+test('renders BlogPostSection with subtitle, paragraph, code and image when all props are provided', () => {
+  render(
+    <BlogPostSection
+      code={`console.log('Hello world!');`}
+      image={{
+        alt_description: 'Test description',
+        light_mode: 'Test-light-mode.png',
+      }}
+      paragraphs={['Test paragraph']}
+      subtitle='Test subtitle'
+    />
+  );
+
+  expect(
+    screen.getByRole('heading', { level: 3, name: 'Test subtitle' })
+  ).toBeInTheDocument();
+  expect(screen.getByRole('paragraph')).toHaveTextContent(/^test paragraph$/i);
+  expect(screen.getByRole('code')).toBeInTheDocument();
+  expect(screen.getByTestId('photo')).toBeInTheDocument();
+});
---
+++ src/app/(routes)/blog/[slug]/components/BlogPostSection/BlogPostSection.tsx
@@ -0,0 +1,40 @@
+import { AspectRatio } from '@/components/shadcn';
+import { Photo } from '@/components/ui';
+
+import { TBlogPostSection } from '@/types/blog-post';
+
+function BlogPostSection({
+  code,
+  image,
+  paragraphs,
+  subtitle,
+}: TBlogPostSection) {
+  return (
+    <div className='space-y-7'>
+      {!!subtitle ? (
+        <h3 className='text-[clamp(1.5rem,5dvw,2rem)] font-semibold leading-[clamp(2rem,5.5dvw,2.25rem)]'>
+          {subtitle}
+        </h3>
+      ) : null}
+      {paragraphs.map((paragraph) => (
+        <p className='just text-pretty break-words text-lg'>{paragraph}</p>
+      ))}
+      {!!code ? (
+        <pre className='overflow-auto rounded-md bg-foreground p-4 text-background'>
+          <code>{code}</code>
+        </pre>
+      ) : null}
+      {!!image ? (
+        <AspectRatio ratio={16 / 9}>
+          <Photo
+            altDescription={image.alt_description}
+            lightMode={image.light_mode}
+            darkMode={image.dark_mode}
+          />
+        </AspectRatio>
+      ) : null}
+    </div>
+  );
+}
+
+export default BlogPostSection;
