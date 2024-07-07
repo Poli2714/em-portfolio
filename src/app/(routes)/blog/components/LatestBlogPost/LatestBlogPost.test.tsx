5c514c1611e05214378d6b503ef52cbcec442e05
Elgun Mehdi <elgun88@gmail.com>
2024-07-07 15:05:20 -0600

    fix: fix a build error caused by unknown issue

--- src/app/(routes)/blog/components/LatestBlogPost/LatestBlogPost.test.tsx
+++ src/app/(routes)/blog/components/LatestBlogPost/LatestBlogPost.test.tsx
@@ -13,12 +13,18 @@ vi.mock('@/components/ui', () => ({
   Photo: () => <div>Mock photo</div>,
 }));
 
-vi.mock('./components/LatestBlogPostHeader/LatestBlogPostHeader', () => ({
-  default: () => <div>Mock blogpost header</div>,
-}));
-
 test('renders LatestBlogPost', () => {
   render(<LatestBlogPost blogPost={__blog_posts__[0]} />);
+  const paragraphs = screen.getAllByRole('paragraph');
+  const link = screen.getByRole('link');
 
   expect(screen.getByTestId('latest-blog-post')).toBeInTheDocument();
+  expect(paragraphs).toHaveLength(2);
+  expect(paragraphs[0]).toHaveTextContent(/^test category1$/i);
+  expect(
+    screen.getByRole('heading', { level: 3, name: /^test title1$/i })
+  ).toBeInTheDocument();
+  expect(link).toBeInTheDocument();
+  expect(link).toHaveAttribute('href', '/blog/test-slug1');
+  expect(paragraphs[1]).toHaveTextContent(/^test description1$/i);
 });
--- src/app/(routes)/blog/components/LatestBlogPost/LatestBlogPost.tsx
+++ src/app/(routes)/blog/components/LatestBlogPost/LatestBlogPost.tsx
@@ -1,8 +1,8 @@
 import { AspectRatio } from '@/components/shadcn';
 import { BlogPostMetaInfo, Photo } from '@/components/ui';
-import LatestBlogPostHeader from './components/LatestBlogPostHeader/LatestBlogPostHeader';
 
 import { TBlogPost } from '@/types/blog-post';
+import Link from 'next/link';
 
 type LatestBlogPostProps = {
   blogPost: TBlogPost;
@@ -19,12 +19,13 @@ function LatestBlogPost({ blogPost }: LatestBlogPostProps) {
         />
       </AspectRatio>
       <div className='flex flex-col gap-y-8 lg:justify-center'>
-        <LatestBlogPostHeader
-          category={blogPost.category}
-          description={blogPost.description}
-          slug={blogPost.slug}
-          title={blogPost.title}
-        />
+        <div className='space-y-4'>
+          <p className='text-sm font-semibold'>{blogPost.category}</p>
+          <h3 className='text-[clamp(1.5rem,4.2dvw,2rem)] font-bold leading-[clamp(2rem,5.25dvw,2.5rem)]'>
+            <Link href={`/blog/${blogPost.slug}`}>{blogPost.title}</Link>
+          </h3>
+          <p className='text-base md:text-lg'>{blogPost.description}</p>
+        </div>
         <BlogPostMetaInfo
           author={blogPost.author}
           date={blogPost.date}
